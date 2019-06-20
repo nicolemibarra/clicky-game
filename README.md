cac7ea96abd0413d23e7cbbcb7a06d5fe17b0b7b
Nicole <nicolemibarra@icloud.com>
2019-06-19 19:58:33 -0700

    Updates

---
+++ README.md
@@ -0,0 +1,9 @@
+# Clicky Game
+
+#### Test your memory with your favorite friends from *Friends*.
+
+#### Click on each character once to win the game. But don't click on a character twice, or it's game over for you!
+
+#### Built with React. Check it out [here](https://nicolemibarra.github.io/clicky-game/).
+
+![screenshot](public/screenshot.png)
---
+++ precache-manifest.b968733832d74c73092a7b72ddec6651.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebe54be784b6521ccb36e26f9d0dcd6",
+    "url": "/repository-clicky-game/index.html"
+  },
+  {
+    "revision": "7f3b94f628b1367197b7",
+    "url": "/repository-clicky-game/static/css/main.daaeae7a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/repository-clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "7f3b94f628b1367197b7",
+    "url": "/repository-clicky-game/static/js/main.860ae77c.chunk.js"
+  },
+  {
+    "revision": "d699f9ec2b755b62ffb6",
+    "url": "/repository-clicky-game/static/js/runtime~main.095802f9.js"
+  }
+]);
